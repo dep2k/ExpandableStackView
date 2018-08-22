bf3b5fc19ffea51f8265d15071e64b6fe538baf5
Deep Arora <deep.arora@DEEP-MAC.local>
2018-08-22 16:49:50 +0530

    Initial Commit

---
+++ TabBarApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabBarApp
+//
+//  Created by Deep Arora on 8/16/18.
+//  Copyright © 2018 Deep Arora. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
